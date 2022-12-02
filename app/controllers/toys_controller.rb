1943521593cacf3f1c869f4a2f6a5a2c7ae63553
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-12-02 16:00:22 +0300

    Completed

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,4 +1,5 @@
 class ToysController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
   wrap_parameters format: []
 
   def index
@@ -8,12 +9,15 @@ class ToysController < ApplicationController
 
   def create
     toy = Toys.create(toy_params)
+    toy = Toy.create!(toy_params)
     render json: toy, status: :created
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
+    toy.update!(toy_params)
+    render json: toy
   end
 
   def destroy
@@ -26,6 +30,10 @@ class ToysController < ApplicationController
   
   def toy_params
     params.permit(:name, :image, :likes)
+  end
+
+  def render_unprocessable_entity(e)
+    render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
   end
 
 end
